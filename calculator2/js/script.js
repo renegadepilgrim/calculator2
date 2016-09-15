06ceb75ecae8e72661520a55a7d88975f0071bd0
Heather Knight and Marty Kovach <marty@>
2016-09-14 19:00:44 -0700

    create calculator form using jquery and html

---
+++ calculator2/js/script.js
@@ -0,0 +1,43 @@
+//business logic:
+var add  = function(number1, number2) {
+      return number1 + number2;
+};
+
+var subtract = function(number1, number2){
+        return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+
+alert(subtract(number1, number2));
+
+var multiply = function(number1, number2){
+        return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(multiply(number1, number2));
+
+var divide = function(number1, number2){
+        return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number1, number2));
+// front-end logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result);
+  });
+});
