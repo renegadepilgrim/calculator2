2cdf7f94c24d0988bfad138271a18788c7a68485
Heather Knight and Marty Kovach <heather@>
2016-09-14 19:32:45 -0700

    add the ability to subtract, multiply and divided. Includes forms for each new functoin, along with the associated jquery and js

--- calculator2/js/script.js
+++ calculator2/js/script.js
@@ -7,29 +7,14 @@ var subtract = function(number1, number2){
         return number1 - number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-
-alert(subtract(number1, number2));
-
 var multiply = function(number1, number2){
         return number1 * number2;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(multiply(number1, number2));
 
 var divide = function(number1, number2){
         return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(divide(number1, number2));
 // front-end logic:
 
 $(document).ready(function() {
@@ -38,6 +23,29 @@ $(document).ready(function() {
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
   var result = add(number1, number2);
-  $("#output").text(result);
+  $("#outputA").text(result);
   });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#subtract1").val());
+  var number2 = parseInt($("#subtract2").val());
+  var result = subtract(number1, number2);
+  $("#outputS").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#multiply1").val());
+  var number2 = parseInt($("#multiply2").val());
+  var result = multiply(number1, number2);
+  $("#outputM").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#divide1").val());
+  var number2 = parseInt($("#divide2").val());
+  var result = divide(number1, number2);
+  $("#outputD").text(result);
+  });
+
 });
